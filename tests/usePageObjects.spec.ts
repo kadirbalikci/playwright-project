56ca49b24b2cae651d3819ea92a6d4816bd24ebf
KADIRS-PC\Kadir BALIKCI <kadir.balikci@mobimeo.com>
2024-04-28 14:48:42 +0200

    no message

---
+++ tests/usePageObjects.spec.ts
@@ -0,0 +1,11 @@
+import {expect, test} from '@playwright/test'
+import {NavigationPage} from '../page-objects/NavigationPage'
+
+test.beforeEach(async({page}, testInfo) => {
+   await page.goto('http://localhost:4200/')
+})
+
+test('navigate to form page', async({page}) => {
+    const navigateTo = new NavigationPage(page)
+    await navigateTo.formLayoutsPage()
+})
