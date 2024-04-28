a33f7d4f7ce90b2fb34b5cfe8cd91f883b2f6b2e
KADIRS-PC\Kadir BALIKCI <kadir.balikci@mobimeo.com>
2024-04-28 15:40:53 +0200

    no message

--- page-objects/NavigationPage.ts
+++ page-objects/NavigationPage.ts
@@ -11,4 +11,32 @@ export class NavigationPage {
         await this.page.getByText('Forms').click()
         await this.page.getByText('Form Layouts').click()
     }
+
+    async datePickerPage(){
+        await this.page.getByText('Forms').click()
+        await this.page.getByText('Datepicker').click()
+    }
+
+    async smartTablePage(){
+        await this.page.getByText('Tables & Data').click()
+        await this.page.getByText('Smart Table').click()
+    }
+
+    async toasterPage(){
+        await this.page.getByText('Modal & Overlays').click()
+        await this.page.getByText('Toastr').click()
+    }
+
+    async tooltipPage(){
+        await this.page.getByText('Modal & Overlays').click()
+        await this.page.getByText('Tooltip').click()
+    }
+    
+    private async selectGroupMenuItem(groupItemTitle: string){
+        const groupMenuItem = this.page.getByTitle(groupItemTitle)
+        const expandStatus = await groupMenuItem.getAttribute('aria-expanded')
+        if (expandStatus == "false") {
+            await groupMenuItem.click()
+        }
+    }
 }
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -8,4 +8,8 @@ test.beforeEach(async({page}, testInfo) => {
 test('navigate to form page', async({page}) => {
     const navigateTo = new NavigationPage(page)
     await navigateTo.formLayoutsPage()
+    await navigateTo.datePickerPage()
+    await navigateTo.smartTablePage()
+    await navigateTo.toasterPage()
+    await navigateTo.tooltipPage()
 })
