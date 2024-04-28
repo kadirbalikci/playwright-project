85c5623bb995ede884bfa3b4fea39d4dda66974f
KADIRS-PC\Kadir BALIKCI <kadir.balikci@mobimeo.com>
2024-04-28 17:49:43 +0200

    before page manager

--- page-objects/NavigationPage.ts
+++ page-objects/NavigationPage.ts
@@ -1,35 +1,46 @@
-import {Page, expect, test} from '@playwright/test'
+import {Locator, Page, expect, test} from '@playwright/test'
 
 export class NavigationPage {
     readonly page: Page
+    readonly fromLayoutsMenuItem: Locator
+    readonly datePickerMenuItem: Locator
+    readonly smartTableMenuItem: Locator
+    readonly toastrMenuItem: Locator
+    readonly tooltipMenuItem: Locator
+
 
     constructor(page: Page){
         this.page = page
+        this.fromLayoutsMenuItem = page.getByText('Form Layouts')
+        this.datePickerMenuItem = page.getByText('Datepicker')
+        this.smartTableMenuItem = page.getByText('Smart Table')
+        this.toastrMenuItem = page.getByText('Toastr')
+        this.tooltipMenuItem = page.getByText('Tooltip')
     }
 
     async formLayoutsPage(){
         await this.page.getByText('Forms').click()
-        await this.page.getByText('Form Layouts').click()
+        await this.fromLayoutsMenuItem.click()
     }
 
     async datePickerPage(){
         await this.page.getByText('Forms').click()
-        await this.page.getByText('Datepicker').click()
+        await this.datePickerMenuItem.click()
     }
 
     async smartTablePage(){
         await this.page.getByText('Tables & Data').click()
-        await this.page.getByText('Smart Table').click()
+        await this.smartTableMenuItem.click()
     }
 
     async toasterPage(){
         await this.page.getByText('Modal & Overlays').click()
-        await this.page.getByText('Toastr').click()
+        await this.toastrMenuItem.click()
     }
 
     async tooltipPage(){
         await this.page.getByText('Modal & Overlays').click()
-        await this.page.getByText('Tooltip').click()
+        await this.tooltipMenuItem.click()
     }
     
     private async selectGroupMenuItem(groupItemTitle: string){
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,7 @@
 import {expect, test} from '@playwright/test'
 import {NavigationPage} from '../page-objects/NavigationPage'
+import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
+import { datePickerPage } from '../page-objects/datepickerPage'
 
 test.beforeEach(async({page}, testInfo) => {
    await page.goto('http://localhost:4200/')
@@ -12,4 +14,17 @@ test('navigate to form page', async({page}) => {
     await navigateTo.smartTablePage()
     await navigateTo.toasterPage()
     await navigateTo.tooltipPage()
+})
+
+test('parameterized methods', async({page}) => {
+    const navigateTo = new NavigationPage(page)
+    const formLayouts = new FormLayoutsPage(page)
+    const onDatePickerPage = new datePickerPage(page)
+
+    await navigateTo.formLayoutsPage()
+    await formLayouts.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 1')
+    await formLayouts.submitInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', false)
+    await navigateTo.datePickerPage()
+    await onDatePickerPage.selectCommonDateFromToday(5)
+    await onDatePickerPage.selectDatepickerWithRangeFromToday(6, 15)
 })
